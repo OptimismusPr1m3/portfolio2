40388ac36ca8aa47d440e9e3a3f82ed35c0c6112
Sebastian Wolff <bastiwolff432@gmail.com>
2024-05-06 19:33:25 +0200

    added pf card for the coming projekt DABubble

--- src/app/pfsection/pfsection.component.ts
+++ src/app/pfsection/pfsection.component.ts
@@ -14,39 +14,43 @@ export class PfsectionComponent {
 
   
   portfolioImages: string[] = [
+    './assets/images/projectframes/dabubble.png',
     '/assets/images/projectframes/Sharkie.png',
     '/assets/images/projectframes/Join.png',
     '/assets/images/projectframes/Pokédex.png',
     //'/assets/images/projectframes/Pollo loco 4.png',
   ];
   portfolioGitLinks: string[] = [
+    '',
     'https://github.com/OptimismusPr1m3/JumpNRun_Sparkie',
     'https://github.com/OptimismusPr1m3/Join',
     'https://github.com/OptimismusPr1m3/Pokedex',
     //'https://github.com/OptimismusPr1m3/JumpNRun_Sparkie',
   ];
   portfolioLiveLinks: string[] = [
+    '',
     'https://sebastian-wolff.developerakademie.net/JumpNRun_SParkie/index.html',
     'https://sebastian-wolff.developerakademie.net/Join/login_index.html',
     'https://sebastian-wolff.developerakademie.net/Pok%C3%A9dex/index.html',
   ];
-  portfolioTitles: string[] = ['Sharkie', 'Join', 'Pokédex', 'Placeholder'];
+  portfolioTitles: string[] = ['DABubble', 'Sharkie', 'Join', 'Pokédex'];
   portfolioInfosEN: string[] = [
+    'Messanger inspired by Slack/Discord. Its still under construction (-:',
     'Take on the role of Sharkie, a brave little white shark, as you navigate through treacherous waters filled with enemies. But beware, the ultimate showdown awaits as you face off against the mighty great white shark.',
     'Welcome to Join, your collaborative Kanban board designed for group productivity! With Join, you can sign up and gain instant access to your personalized task dashboard, where you will find all your assignments neatly organized.',
     'Introducing my Pokédex, powered by the PokeAPI! Explore, search, and learn about Pokémon with ease. Dive into stats and attacks for each Pokémon. The ultimate companion for Pokémon enthusiasts!',
-    //'Placeholder',
   ];
   portfolioInfosDE: string[] = [
+    'Nachrichtendienst inspiriert von Slack/Discord. Aktuell noch in der Entwicklung (-:',
     'Nimm die Rolle von Sharkie ein, einem mutigen kleinen Weißen Hai, während du dich durch gefährliche Gewässer voller Feinde bewegst. Doch sei gewarnt, das ultimative Duell erwartet dich, wenn du dem mächtigen Weißen Hai gegenüberstehst.',
     'Willkommen bei Join, Ihrem kollaborativen Kanban-Board, das für Gruppenproduktivität entwickelt wurde! Mit Join können Sie sich anmelden und sofort Zugriff auf Ihr personalisiertes Aufgaben-Dashboard erhalten, auf dem Sie alle Ihre Aufgaben ordentlich organisiert finden.',
     'Hier ist mein Pokédex, angetrieben von der PokeAPI! Erkunde, suche und lerne Pokémon spielend leicht kennen. Tauche ein in Statistiken und Attacken für jedes Pokémon. Der ultimative Begleiter für Pokémon-Enthusiasten!',
-    'Platzhalter',
   ]
   portfolioTech: string[] = [
-    'Javascript | HTML | CSS',
-    'Javascript | HTML | CSS',
-    'Javascript | HTML | CSS | API',
+    'TypeScript | Firebase | SCSS | Realtime Database',
+    'JavaScript | HTML | CSS',
+    'JavaScript | HTML | CSS',
+    'JavaScript | HTML | CSS | API',
     //'TypeScript | HTML | Firebase | CSS',
   ];
 
